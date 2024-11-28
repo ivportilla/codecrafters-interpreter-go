915f4bcc5ee63d1d457006cb5d57eb16e03dc693
Ivan Portilla <jcuastumal@gmail.com>
2024-11-28 18:23:04 -0500

    codecrafters submit [skip ci]

--- cmd/myinterpreter/parser.go
+++ cmd/myinterpreter/parser.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 )
@@ -16,6 +17,9 @@ type NumberLit struct {
 }
 type StringLit struct {
 	Value string
+}
+type Grouping struct {
+	Value Expr
 }
 type Nil struct{}
 
@@ -53,6 +57,10 @@ func NewLiteral(token Token) (Expr, error) {
 	}
 }
 
+func NewGrouping(expr Expr) Expr {
+	return &Grouping{expr}
+}
+
 func (boolExpr *Boolean) Print() string {
 	return when(boolExpr.Value, "true", "false")
 }
@@ -64,6 +72,8 @@ func (nilExpr *Nil) Print() string {
 func (numberExpr *NumberLit) Print() string { return formatFloatNumber(numberExpr.Value) }
 
 func (stringExpr *StringLit) Print() string { return stringExpr.Value }
+
+func (grouping *Grouping) Print() string { return "(group " + grouping.Value.Print() + ")" }
 
 func printAST(expr Expr) string {
 	return expr.Print()
@@ -78,12 +88,66 @@ func (p *Parser) currentToken() Token {
 	return p.tokens[p.current]
 }
 
-func (p *Parser) MatchLiteral() (Expr, error) {
-	return NewLiteral(p.currentToken())
+func (p *Parser) check(tokenType TokenType) bool {
+	return p.tokens[p.current].tokenType == tokenType
+}
+
+func (p *Parser) match(tokenType TokenType) bool {
+	if p.check(tokenType) {
+		p.advance()
+		return true
+	}
+
+	return false
+}
+
+func (p *Parser) nextToken() Token {
+	p.advance()
+	return p.tokens[p.current]
+}
+
+func (p *Parser) previousToken() Token {
+	return p.tokens[p.current-1]
+}
+
+func (p *Parser) advance() Token {
+	if !p.isAtEnd() {
+		p.current++
+	}
+	return p.previousToken()
+}
+
+func (p *Parser) consume(tokenType TokenType, errorMsg string) error {
+	if !p.match(tokenType) {
+		return errors.New(errorMsg)
+	}
+	return nil
+}
+
+func (p *Parser) isAtEnd() bool {
+	return p.currentToken().tokenType == EOF
+}
+
+func (p *Parser) MatchPrimary() (Expr, error) {
+	if p.match(LeftParen) {
+		expr, err := p.MatchExpr()
+		if err != nil {
+			return nil, err
+		}
+		err = p.consume(RightParen, "Expect ')' after expression.")
+		if err != nil {
+			return nil, err
+		}
+		return NewGrouping(expr), nil
+	} else {
+		lit, err := NewLiteral(p.currentToken())
+		p.advance()
+		return lit, err
+	}
 }
 
 func (p *Parser) MatchExpr() (Expr, error) {
-	return p.MatchLiteral()
+	return p.MatchPrimary()
 }
 
 func parse(tokens []Token) {
